3c786fc41e87bb87c99f4e1f16e77a31581cc534
raoSahab14127 <eb20103109.raohammadali@gmail.com>
2024-05-20 06:01:30 -0700

    custom modal working

---
+++ cumtafapi/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import customUserManager
+class CustomUser(AbstractUser):
+    username = None
+    phone_number = models.CharField(max_length=100, unique=True)
+    age = models.IntegerField(null=True, blank=True)
+    city = models.CharField(max_length=100,null=True,blank=True)
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = []
+
+    objects = customUserManager()
